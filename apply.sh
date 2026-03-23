935d5e6d96f051104193d87d583392331bced7a6
Bartosz Sadecki <bartosz.sadecki@posteo.pl>
2026-03-23 15:47:05 +0100

    switch back to yabai from aerospace

--- apply.sh
+++ apply.sh
@@ -7,4 +7,4 @@ dir="$(dirname "$(realpath $0)")"
 mkdir -p ~/.config
 ln -sf "$dir/kitty" ~/.config/
 ln -sf "$dir/skhdrc" ~/.skhdrc
-ln -sf "$dir/aerospace.toml" ~/.aerospace.toml
+ln -sf "$dir/yabairc" ~/.yabairc
